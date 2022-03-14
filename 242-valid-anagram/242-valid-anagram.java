cbe200024faddcf9bc115dd3330884bbbb8af627
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-03-14 17:59:34 -0400

    Time: 13 ms (29.61%), Space: 46.9 MB (6.42%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> so = new HashMap<>();
+        HashMap<Character, Integer> ta = new HashMap<>();
+        
+        for(char a: s.toCharArray()){
+            so.put(a, so.getOrDefault(a, 0)+1);
+        }
+        
+        for(char a: t.toCharArray()){
+            ta.put(a, ta.getOrDefault(a, 0)+1);
+        }
+        
+        if(so.equals(ta))return true;
+        return false;
+    }
+}
