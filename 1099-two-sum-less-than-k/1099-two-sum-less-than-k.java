4fc0770aa191ae49257c71679222a42d187602c2
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-04-02 16:34:54 -0400

    Time: 1 ms (100.00%), Space: 40.2 MB (97.63%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
@@ -1,12 +1,30 @@
+// class Solution {
+//     public int twoSumLessThanK(int[] nums, int k) {
+//         int n = -1;
+//         for(int i = 0; i<nums.length-1;i++){
+//             if(nums[i]>k) continue;
+//             for(int j=i+1; j<nums.length; j++){
+//                 if(nums[j]>k) continue;
+//                 if(nums[i]+nums[j]>n && nums[i]+nums[j]<k ) n = nums[i]+nums[j];
+//             }
+//         }
+//         return n;
+//     }
+// }
+
 class Solution {
+
     public int twoSumLessThanK(int[] nums, int k) {
+        Arrays.sort(nums);
+        
         int n = -1;
-        for(int i = 0; i<nums.length-1;i++){
-            if(nums[i]>k) continue;
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[j]>k) continue;
-                if(nums[i]+nums[j]>n && nums[i]+nums[j]<k ) n = nums[i]+nums[j];
-            }
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] < k) {
+                n = Math.max(n,nums[i] + nums[j]);
+                i++;
+            } else j--;
         }
         return n;
     }
