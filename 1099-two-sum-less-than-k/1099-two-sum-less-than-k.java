521a92e5dcefa2ce28434421d8deb5f80af385c9
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-04-02 15:48:14 -0400

    Time: 4 ms (23.26%), Space: 43.4 MB (6.37%) - LeetHub

---
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int twoSumLessThanK(int[] nums, int k) {
+        int n = -1;
+        for(int i = 0; i<nums.length-1;i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]>n && nums[i]+nums[j]<k ) n = nums[i]+nums[j];
+            }
+        }
+        return n;
+    }
+}
