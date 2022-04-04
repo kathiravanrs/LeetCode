cffe7c30fc015e516907b435db5abfa9f74886d3
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-04-04 11:22:57 -0400

    Time: 4 ms (17.13%), Space: 44.2 MB (78.07%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.java
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -1,21 +1,44 @@
 class Solution {
+
+    //     public int shortestDistance(String[] wordsDict, String word1, String word2) {
+    //         List<Integer> f = new ArrayList<>();
+    //         List<Integer> s = new ArrayList<>();
+
+    //         int i=-1;
+    //         for(String w : wordsDict){
+    //             i++;
+    //             if(w.equals(word1)) f.add(i);
+    //             if(w.equals(word2)) s.add(i);
+    //         }
+    //         Set<Integer> set = new TreeSet<>();
+
+    //         for(int k: f){
+    //             for(int j: s){
+    //                 set.add(Math.abs(k-j));
+    //             }
+    //         }
+    //         return (int)set.toArray()[0];
+    //     }
+
     public int shortestDistance(String[] wordsDict, String word1, String word2) {
-        List<Integer> f = new ArrayList<>();
-        List<Integer> s = new ArrayList<>();
-        
-        int i=-1;
-        for(String w : wordsDict){
-            i++;
-            if(w.equals(word1)) f.add(i);
-            if(w.equals(word2)) s.add(i);
-        }
-        Set<Integer> set = new TreeSet<>();
-        
-        for(int k: f){
-            for(int j: s){
-                set.add(Math.abs(k-j));
+        int p1 = 0;
+        int p2 = 0;
+        int res = Integer.MAX_VALUE;
+        boolean found1 = false;
+        boolean found2 = false;
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
+                p1 = i;
+                found1 = true;
+            }
+            if (wordsDict[i].equals(word2)) {
+                p2 = i;
+                found2 = true;
+            }
+            if(found1&&found2){
+                res = Math.min(res, Math.abs(p1-p2));
             }
         }
-        return (int)set.toArray()[0];
+        return res;
     }
 }
