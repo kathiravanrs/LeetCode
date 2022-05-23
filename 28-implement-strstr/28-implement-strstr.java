37263986a6b54172511879cd30118c7328cdb746
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-05-23 13:19:48 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (43.54%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+//         if(!haystack.contains(needle)){
+//             return -1;
+//         }
+//         if(needle.length()==1){
+//             return haystack.get
+//         }
+        
+//         int c = 0;
+//         while(haystack.substring(c).contains(needle)){
+//             c++;
+//         }
+//         return c-1;
+        
+    }
+}
