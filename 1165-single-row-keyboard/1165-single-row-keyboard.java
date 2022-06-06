0d1cf9d4f1fee70663c7fb6d0edbf6d5f2321f95
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-06-06 18:50:06 -0400

    Time: 6 ms (69.21%), Space: 42.3 MB (72.77%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.java
+++ 1165-single-row-keyboard/1165-single-row-keyboard.java
@@ -1,10 +1,11 @@
 class Solution {
     public int calculateTime(String keyboard, String word) {
         int time = 0;
-        int pos = 0;
+        int prev = 0;
         for(char a:word.toCharArray()){
-            time += Math.abs(pos - keyboard.indexOf(a));
-            pos = keyboard.indexOf(a);
+            int pos = keyboard.indexOf(a);
+            time += Math.abs(prev - pos);
+            prev = pos;
             
         }
         return time;
