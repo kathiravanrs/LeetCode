214ab4ea41bfe97536f9f63f5767d1493a157d84
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-06-29 12:59:06 -0400

    Time: 8 ms (35.81%), Space: 42.9 MB (11.97%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,22 +1,36 @@
 class Solution {
     public int longestPalindrome(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char c =s.charAt(i);
-            map.put(c, map.getOrDefault(c,0)+1);
-        }        
         int res = 0;
-        boolean odd = false;
-        for(int v:map.values()){
-            res+=v;
-            if(v%2!=0)
-            {
-                if(!odd) res++;
-                res--;
-                odd = true;
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i<s.length();i++){
+            char c = s.charAt(i);
+            if(set.contains(c)){
+                res+=2;
+                set.remove(c);
+            }
+            else{
+                set.add(c);
             }
         }
-        
+        if(!set.isEmpty()) res++;
         return res;
+//         Map<Character, Integer> map = new HashMap<>();
+//         for(int i=0;i<s.length();i++){
+//             char c =s.charAt(i);
+//             map.put(c, map.getOrDefault(c,0)+1);
+//         }        
+//         int res = 0;
+//         boolean odd = false;
+//         for(int v:map.values()){
+//             res+=v;
+//             if(v%2!=0)
+//             {
+//                 if(!odd) res++;
+//                 res--;
+//                 odd = true;
+//             }
+//         }
+        
+//         return res;
     }
 }
