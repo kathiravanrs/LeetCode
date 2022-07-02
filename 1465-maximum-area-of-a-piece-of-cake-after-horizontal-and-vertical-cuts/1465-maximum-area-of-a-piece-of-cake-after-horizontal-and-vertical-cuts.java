18c4ea0206fb3eb7e9dadf21acfb2ec96d7ebe3d
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-07-02 16:10:59 -0400

    Time: 237 ms (5.10%), Space: 83 MB (5.21%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,13 +1,38 @@
 class Solution {
-    public int maxArea(int h, int w, int[] hCuts, int[] vCuts) {
-    Arrays.sort(hCuts);
-    Arrays.sort(vCuts);
-    int max_h = Math.max(hCuts[0], h - hCuts[hCuts.length - 1]);
-    int max_v = Math.max(vCuts[0], w - vCuts[vCuts.length - 1]);  
-    for (int i = 0; i < hCuts.length - 1; ++i)
-        max_h = Math.max(max_h, hCuts[i + 1] - hCuts[i]);
-    for (int i = 0; i < vCuts.length - 1; ++i)
-        max_v = Math.max(max_v, vCuts[i + 1] - vCuts[i]);        
-    return (int)((long)max_h * max_v % 1000000007);        
-}
+
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        List<Long> hz = new ArrayList<>();
+        List<Long> vr = new ArrayList<>();
+        hz.add(0l);
+        vr.add(0l);
+        for (int a : horizontalCuts) {
+            hz.add((long) a);
+        }
+        for (int a : verticalCuts) {
+            vr.add((long) a);
+        }
+        hz.add((long) h);
+        vr.add((long) w);
+
+        Collections.sort(hz);
+        Collections.sort(vr);
+
+        int max = 0;
+        long ml = 0;
+        long mw = 0;
+        System.out.println(hz);
+        System.out.println(vr);
+        for (int i = 0; i < hz.size() - 1; i++) {
+            long l = hz.get(i + 1) - hz.get(i);
+            ml = Math.max(ml, l);
+        }
+        for (int j = 0; j < vr.size() - 1; j++) {
+            long b = vr.get(j + 1) - vr.get(j);
+            mw = Math.max(mw, b);
+            // int area =((int) (l*b)%(1000000007));
+            // max = Math.max(max, area);
+        }
+        System.out.println(ml + " " + mw);
+        return (int) ((ml * mw) % 1000000007);
+    }
 }
