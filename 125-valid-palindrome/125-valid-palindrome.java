af641a62c292cf62125b7fcdd3afb94e7afd29dc
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-07-05 16:01:11 -0400

    Time: 592 ms (32.94%), Space: 43.5 MB (66.69%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -2,14 +2,16 @@ class Solution {
     public boolean isPalindrome(String s) {
         if(s.isBlank()) return true;
         String t = "";
+        String r = "";
         String valid = "abcdefghijklmnopqrstuvwxyz0123456789";
         for(char a: s.toLowerCase().toCharArray()){
             if(valid.contains(String.valueOf(a))) t+=String.valueOf(a);
         }
-        for(int i = 0;i<t.length()/2;i++){
-           if(t.charAt(i)!=t.charAt(t.length()-1-i)) return false;
+        for(int i = t.length()-1;i>=0;i--){
+            r += String.valueOf(t.charAt(i));
         }
 
-        return true;
+        if(t.equals(r)) return true;
+        else return false;
     }
 }
