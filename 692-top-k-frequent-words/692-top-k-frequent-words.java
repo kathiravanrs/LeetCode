856a261bc7aeacbabb8d067dc29ecfea045d637b
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-07-18 15:55:06 -0400

    Time: 15 ms (13.70%), Space: 46.3 MB (5.23%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> ans = new ArrayList<>();
+        Map<String,Integer> map = new HashMap<>();
+        for(String s: words){
+            map.put(s, map.getOrDefault(s, 0)+1);
+        }
+        Map<Integer, Set<String>> countMap = new TreeMap<>(Collections.reverseOrder());
+        for(String s: map.keySet()){
+            int count = map.get(s);
+            Set<String> set = countMap.getOrDefault(count, new TreeSet<>());
+            set.add(s);
+            countMap.put(count, set);
+        }
+        System.out.println(countMap);
+        
+        for(Set<String> set: countMap.values()){
+            for(String s: set) {
+                ans.add(s);
+                k--;
+                if(k==0) return ans;
+
+            }
+        }
+        
+        return ans;   
+    }
+}
