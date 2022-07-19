ee062341ffda476c405d394bc293f0d6d5faad1d
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-07-19 12:24:09 -0400

    Time: 2 ms (87.01%), Space: 48.4 MB (24.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,17 +1,10 @@
-
 public class Solution {
-
     public int[] countBits(int n) {
 
         int[] ans = new int[n + 1];
-
-        for (int x = 1; x <= n; ++x) {
-
-            // x / 2 is x >> 1 and x % 2 is x & 1
-
-            ans[x] = ans[x >> 1] + (x & 1); 
+        for (int x = 0; x <= n; x++) {
+            ans[x] = Integer.bitCount(x);
         }
-
         return ans;
     }
 }
