9c1952a90199c57ac2e4c31418558e661e10002b
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-07-28 09:53:53 -0400

    Time: 3 ms (95.30%), Space: 43.7 MB (57.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
         int[] count = new int[26];
         for(char a: s.toCharArray()) count[a-'a']++;
         for(char a: t.toCharArray()) count[a-'a']--;
