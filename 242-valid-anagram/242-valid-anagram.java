f680f479d7dfb1d589cea2157ce393f7311f74ae
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-07-28 09:43:07 -0400

    Time: 21 ms (25.89%), Space: 46.7 MB (12.94%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
         HashMap<Character, Integer> so = new HashMap<>();
         HashMap<Character, Integer> ta = new HashMap<>();
         
