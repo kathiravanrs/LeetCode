a1378c94c227a372e6d3188818d618da41eeb5ed
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-10-11 10:53:41 -0400

    Time: 4 ms (59.53%), Space: 91.8 MB (73.77%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,18 +1,26 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        Set<Integer> s  = new HashSet<>();
-        for(int a: nums) s.add(a);
-        if(s.size()<3) return false;
-        int l = nums.length;
-        for(int i=1;i<l-1;i++){
-            boolean flag = false;
-            for(int j=i-1;j>=0;j--){
-                if(nums[j]<nums[i]){flag = true; break;}
-            }
-            if(!flag) continue;
-            for(int k=i+1;k<l;k++){
-                if(nums[k]>nums[i]) return true;
-            }
+        // Set<Integer> s  = new HashSet<>();
+        // for(int a: nums) s.add(a);
+        // if(s.size()<3) return false;
+        // int l = nums.length;
+        // for(int i=1;i<l-1;i++){
+        //     boolean flag = false;
+        //     for(int j=i-1;j>=0;j--){
+        //         if(nums[j]<nums[i]){flag = true; break;}
+        //     }
+        //     if(!flag) continue;
+        //     for(int k=i+1;k<l;k++){
+        //         if(nums[k]>nums[i]) return true;
+        //     }
+        // }
+        // return false;
+        int f = Integer.MAX_VALUE;
+        int s = Integer.MAX_VALUE;
+        for(int a: nums){
+            if(a<=f) f = a;
+            else if(a<=s) s = a;
+            else return true;
         }
         return false;
     }
