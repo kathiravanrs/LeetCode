a5dd8a7e2e9940f15c5bf3489bbe07668ebd13d9
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-10-11 10:47:56 -0400

    Time: 80 ms (5.02%), Space: 94 MB (23.87%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        Set<Integer> s  = new HashSet<>();
+        for(int a: nums) s.add(a);
+        if(s.size()<3) return false;
+        int l = nums.length;
+        for(int i=1;i<l-1;i++){
+            boolean flag = false;
+            for(int j=i-1;j>=0;j--){
+                if(nums[j]<nums[i]){flag = true; break;}
+            }
+            if(!flag) continue;
+            for(int k=i+1;k<l;k++){
+                if(nums[k]>nums[i]) return true;
+            }
+        }
+        return false;
+    }
+}
