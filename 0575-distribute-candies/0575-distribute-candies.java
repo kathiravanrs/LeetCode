a39e7dcd504883077729b8a1c9002c013068d265
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-10-12 19:44:36 -0400

    Time: 36 ms (92.64%), Space: 44.7 MB (92.98%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -3,7 +3,8 @@ class Solution {
         int l = candyType.length;
         Set<Integer> set = new HashSet<>();
         for(int i=0;i<l;i++) set.add(candyType[i]);
-        
-        return Math.min(set.size(), l/2);
+        int s = set.size();
+        if(l==s) return l/2;
+        return Math.min(s, l/2);
     }
 }
