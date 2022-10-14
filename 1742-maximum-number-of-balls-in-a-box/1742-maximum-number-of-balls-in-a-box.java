38b1917abbc3b99a82414ee27cd7d02cc3a416c4
Kathiravan Sekar <30983831+kathiravanrs@users.noreply.github.com>
2022-10-14 14:08:36 -0400

    Time: 32 ms (73.21%), Space: 41.9 MB (48.07%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.java
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.java
@@ -1,21 +1,15 @@
 class Solution {
     public int countBalls(int lowLimit, int highLimit) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] cnt = new int[46]; 
         int max = 0;
-        for(int i = lowLimit;i<=highLimit;i++){
-            int val = dSum(i);
-            map.put(val, map.getOrDefault(val,0)+1);
-            max = Math.max(max, map.get(val));
+        for (int i = lowLimit; i <= highLimit; ++i) {
+            int num = i, sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            max = Math.max(++cnt[sum], max);
         }
         return max;
-    }
-    
-    public int dSum(int n){
-        int sum = 0;
-        while(n>0){
-            sum+= n%10;
-            n = n/10;
-        }
-        return sum;
     }
 }
